040dfd27f9540e00d8cd0f5315a134b9422a1fec
marline2000 <jihyun981227@naver.com>
2022-01-26 00:50:53 +0900

    Create project

---
+++ main.js
@@ -0,0 +1,11 @@
+const str = `
+013-2020-2020
+dkwldk@gmail.cam
+https://www.dddd.com/wmdkwkd?dfwedf
+The quick browm fox jumps over the dog.
+abbcccdddd
+`
+
+// const regexp = new RegExp('dk','g')
+
+console.log(str.match(/\.$/gim))
